b3a7e5541a3e1b2f76fafd8adbb993e1251cb9b5
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 21:44:57 +0200

    Added OldNumbersUseCase

---
+++ ATTApp.UseCase/OldNumbersUseCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.UseCase
+{
+    public class OldNumbersUseCase
+    {
+        private static Random random = new Random();
+
+        public void GenerateOddNumbers(ConcurrentBag<int> globalVariable, int limit)
+        {
+            while (globalVariable.Count < limit)
+            {
+                int number = random.Next(1, int.MaxValue);
+                if (number % 2 != 0)
+                {
+                    globalVariable.Add(number);
+                }
+            }
+        }
+    }
+}
