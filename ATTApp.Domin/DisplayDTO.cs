fcfc643a8727179e216aa6c2e25e8e46711c1200
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 21:52:10 +0200

    Added a Domain Class Lib and added DisplayDTO

---
+++ ATTApp.Domin/DisplayDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.Domin
+{
+    public class DisplayDTO
+    {
+        public int TotalCount { get; set; }
+        public int OldCount { get; set; }
+        public int EventCount { get; set; }
+    }
+}
