7bd070b45666e456303a1f6fa977f991d1194913
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 22:12:44 +0200

    Created SaveNumberUseCase

---
+++ ATTApp.UseCase/SaveNumberUseCase.cs
@@ -0,0 +1,26 @@
+﻿using ATTApp.Repository.Contracts;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.UseCase
+{
+    public  class SaveNumberUseCase
+    {
+
+
+        public INumberRepository INumberRepository {  get; set; }
+        public SaveNumberUseCase(INumberRepository iNumberRepository)
+        {
+            INumberRepository = iNumberRepository;
+        }
+
+        public bool SaveData(ConcurrentBag<int> globalVariable)
+        {
+            return INumberRepository.Add(globalVariable);
+        }
+    }
+}
