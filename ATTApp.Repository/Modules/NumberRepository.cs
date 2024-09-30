e7c0785f8fc7df940a39ada9afcfff728ab43c18
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 22:12:28 +0200

    Update Repo Code

---
+++ ATTApp.Repository/Modules/NumberRepository.cs
@@ -0,0 +1,57 @@
+﻿using ATTApp.Data.Models;
+using ATTApp.Repository.Contracts;
+using ATTApp.Shared;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.Repository.Modules
+{
+    public class NumberRepository : INumberRepository
+    {
+        public attContext context { get; set; }
+        public IsPrime IsPrime { get; set; }
+
+        public List<Number> numClass = new List<Number>();
+
+        public NumberRepository(attContext context, IsPrime isPrime)
+        {
+            this.context = context;
+            IsPrime = isPrime;
+        }
+
+        public bool Add(ConcurrentBag<int> list)
+        {
+            try
+            {
+                context.AddRange(CreateObject(list));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        private List<Number> CreateObject(ConcurrentBag<int> list)
+        {
+            foreach (var item in list)
+            {
+                bool isPrime = IsPrime.Check(Math.Abs(item));
+                numClass.Add(
+                    new Number
+                    {
+                        IsPrime = isPrime,
+                        Value = item
+                    }
+                    );
+            }
+
+            return numClass;
+        }
+    }
+}
---
+++ ATTApp.UseCase/LogicUseCase.cs
@@ -0,0 +1,46 @@
+﻿using ATTApp.Domin;
+using ATTApp.Repository.Contracts;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ATTApp.UseCase
+{
+    public  class LogicUseCase
+    {
+        public OldNumbersUseCase OldNumbersUseCase { get; set; }
+        public PrimeNumbersUseCase PrimeNumbersUseCase { get; set; }
+        public EvenNumbersUseCase EvenNumbersUseCase { get; set; }
+        public SortingUseCase SortingUseCase { get; set; }
+
+        private static ConcurrentBag<int> globalVariable = new ConcurrentBag<int>();
+
+        public LogicUseCase(OldNumbersUseCase oldNumbersUseCase, PrimeNumbersUseCase primeNumbersUseCase, EvenNumbersUseCase evenNumbersUseCase, SortingUseCase sortingUseCase)
+        {
+            OldNumbersUseCase = oldNumbersUseCase;
+            PrimeNumbersUseCase = primeNumbersUseCase;
+            EvenNumbersUseCase = evenNumbersUseCase;
+            SortingUseCase = sortingUseCase;
+        }
+
+        public DisplayDTO Start()
+        {
+
+
+            Task oddNumberTask = Task.Run(() => OldNumbersUseCase.GenerateOddNumbers(globalVariable, 2500000));
+            Task primeNumberTask = Task.Run(() => PrimeNumbersUseCase.GeneratePrimeNumbers(globalVariable, 2500000));
+
+            Task.WaitAll(oddNumberTask, primeNumberTask);
+
+            Task evenNumberTask = Task.Run(() => EvenNumbersUseCase.GenerateEvenNumbers(globalVariable, 10000000));
+            evenNumberTask.Wait();
+            return SortingUseCase.SortAndDisplayResults(globalVariable);
+        }
+
+
+    }
+}
