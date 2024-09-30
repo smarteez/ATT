6cfa20cd97da12107417b731be8967a359d4127b
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 21:42:29 +0200

    Created EvenNumbersUseCase

---
+++ ATTApp.UseCase/EvenNumbersUseCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.UseCase
+{
+    public class EvenNumbersUseCase
+    {
+        private static Random random = new Random();
+
+        public void GenerateEvenNumbers(ConcurrentBag<int> globalVariable, int limit)
+        {
+            while (globalVariable.Count < limit)
+            {
+                int number = random.Next(1, int.MaxValue);
+                if (number % 2 == 0)
+                {
+                    globalVariable.Add(number);
+                }
+            }
+        }
+    }
+}
