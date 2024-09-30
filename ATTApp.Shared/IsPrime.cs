ef1607841c9978d65713da846cdcaf9ce02fad01
Donny Singh <d.singh.zn@gmail.com>
2024-09-30 21:47:45 +0200

    Created a Shared section for IsPrime

---
+++ ATTApp.Shared/IsPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.Shared
+{
+    public class IsPrime
+    {
+
+        public bool Check(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
