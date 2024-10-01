c25804143b8dff3cf04023cb41813b1d52d29faf
Donny Singh <d.singh.zn@gmail.com>
2024-10-01 14:55:15 +0200

    Added a hepper class for ListToBinary

---
+++ ATTApp.Shared/ListToBinary.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTApp.Shared
+{
+    public  class ListToBinary
+    {
+        public static byte[] Convert<T>(T obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            return MessagePackSerializer.Serialize(obj);
+        }
+    }
+}
