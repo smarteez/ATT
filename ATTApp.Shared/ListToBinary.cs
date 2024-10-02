68895b6658ccaa02d7c392b2cbf76e4b6831b3db
Donny Singh <d.singh.zn@gmail.com>
2024-10-02 13:33:52 +0200

    Update the helper class to convert to Binary

--- ATTApp.Shared/ListToBinary.cs
+++ ATTApp.Shared/ListToBinary.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ATTApp.Shared
 {
     public  class ListToBinary
     {
-        public static byte[] Convert<T>(T obj)
+        public static byte[] Convert<T>(List<T> list)
         {
-            if (obj == null)
-                throw new ArgumentNullException(nameof(obj));
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
 
-            return MessagePackSerializer.Serialize(obj);
+            return JsonSerializer.SerializeToUtf8Bytes(list);
         }
     }
 }
